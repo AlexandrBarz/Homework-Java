4ef28dfaa3e146e8627a8ad438ef6bd6c9bd4d29
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2023-02-19 23:31:44 +0300

    corrected file Homework21

--- Homework21.java
+++ Homework21.java
@@ -1,11 +1,9 @@
 // Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+import java.io.IOException;
+import java.util.logging.*;
 
 public class Homework21 {
 
@@ -19,76 +17,40 @@ public class Homework21 {
         System.out.println("");
         return array;
     }
-    randomArray();
+
+    static int[] bubblesSort(int arr[]) throws IOException {
+        Logger logger = Logger.getLogger(Homework21.class.getName());
+        FileHandler fHandler = new FileHandler("logHW21.txt", true);
+        logger.addHandler(fHandler);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fHandler.setFormatter(sFormat);;
+
+        int temp;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            logger.info(Arrays.toString(arr));
+        }
+        return arr;
+    }
+
+    static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        System.out.println("Исходный массив:");
+        int[] inputArray = randomArray();
+        System.out.println();
+        int[] outputArray = bubblesSort(inputArray);
+        System.out.println();
+        System.out.println("Отсортированный массив в порядке в порядке убывания:");
+        print(outputArray);        
+    }
 }
-//     static int[] bubblesSort(int arr[]) throws IOException {
-//         Logger loger = Logger.getLogger(Homework21.class.getName());
-//         FileHandler fHandler = new FileHandler("Homework21.txt");
-//         SimpleFormatter sFormatter = new SimpleFormatter();
-//         fHandler.setFormatter(sFormatter);
-//         loger.addHandler(fHandler);
-
-//         int temp;
-//         for (int i = arr.length - 1; i >= 0; i--) {
-//             for (int j = 0; j < arr.length - 1; j++) {
-//                 if (arr[j] < arr[j + 1]) {
-//                     temp = arr[j];
-//                     arr[j] = arr[j + 1];
-//                     arr[j + 1] = temp;
-//                 }
-//             }
-//             loger.info(Arrays.toString(arr));
-//         }
-//         return arr;
-//     }
-
-//     static void print(int[] arr) {
-//         for (int i = 0; i < arr.length; i++) {
-//             System.out.print(arr[i] + " ");
-//         }
-//     }
-
-//     public static void main(String[] args) {
-//         int[] inputArray = randomArray();
-//         int[] outputArray = bubblesSort(inputArray);
-//         print(outputArray);
-//     }
-// }
-
-
-
-
-
-
-
-// import java.io.IOException;
-// import java.util.logging.*;
-// public class Homework21 {
-// public static void main(String[] args) throws IOException {
-// Logger logger = Logger.getLogger(Homework21.class.getName());
-// FileHandler fh = new FileHandler("logTask2.xml");
-// logger.addHandler(fh);
-// XMLFormatter xml = new XMLFormatter();
-// fh.setFormatter(xml);
-// logger.info("Sort Array");
-// int [] array = {2, 5, 1, -7, 3, 64, 0};
-// lib.printArrayInt(array);
-// int[] newArr = listSort(array);
-// System.out.printf("\n Отсортированный массив: \n");
-// lib.printArrayInt(newArr);
-// }
-// public static int[] listSort(int [] arr){
-// for(int k = 0; k < arr.length-1; k++) {
-
-// for (int i = 0; i < arr.length - k-1; i++){
-// if (arr[i] > arr[i+1]) {
-// int temp = arr[i];
-// arr[i] = arr[i+1];
-// arr[i+1] = temp;
-// }
-// }
-// }
-// return arr;
-// }
-
-// }
