3ddda39d68ec01786d70f6141de1acd391cd61e1
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2023-02-20 23:14:58 +0300

    added file homework31

---
+++ homework31.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+// Реализовать алгоритм сортировки слиянием
+
+public class homework31 {
+    public static void main(String[] args) {
+        System.out.println("Исходный массив:");
+        int[] a = randomArr();
+        System.out.println("Отсортированный массив:");
+        mergeSort(a, a.length);
+        for (int i = 0; i < a.length; i++)
+        System.out.print(a[i] + " ");
+    }
+
+    public static int[] randomArr() {
+        Random rand = new Random();
+        int arr[] = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+        return arr;
+    }
+
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2)
+            return;
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+        merge(a, l, r, mid, n - mid);
+    }
+
+    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j])
+                a[k++] = l[i++];
+            else
+                a[k++] = r[j++];
+        }
+        while (i < left)
+            a[k++] = l[i++];
+        while (j < right)
+            a[k++] = r[j++];
+    }
+}
