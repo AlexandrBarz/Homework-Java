63e3e48a2f1777b03c60130ea7b44b65b9a4e82c
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2023-03-02 01:34:17 +0300

    corrected file homework53

--- homework53.java
+++ homework53.java
@@ -1,3 +1,59 @@
+// Реализовать алгоритм пирамидальной сортировки (HeapSort).
+
+import java.util.Random;
+
 public class homework53 {
-    
+    public void sort(int array[]) {
+        int n = array.length;
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(array, n, i);
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            heapify(array, i, 0);
+        }
+    }
+
+    void heapify(int array[], int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < n && array[l] > array[largest])
+            largest = l;
+        if (r < n && array[r] > array[largest])
+            largest = r;
+        if (largest != i) {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+            heapify(array, n, largest);
+        }
+    }
+
+    static int[] randomArray() {
+        Random rand = new Random();
+        int array[] = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(100);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("");
+        return array;
+    }
+
+    static void printArray(int array[]) {
+        for (int i = 0; i < array.length; ++i)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Исходный массив:");
+        int array[] = randomArray();
+        homework53 ob = new homework53();
+        ob.sort(array);
+        System.out.println("Отсортированный массив:");
+        printArray(array);
+    }
 }
